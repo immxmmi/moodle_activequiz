b899303bcd67c3e3ad651786b0bfa803de713187
Momo <immxmmi@gmx.at>
2022-06-17 16:16:54 +0200

    try reload func

--- js/instructor.js
+++ js/instructor.js
@@ -820,10 +820,13 @@ function update_chart(chart_typ){
         }).done(function (results) {
                 myChart.config._config.data = results.data.chartdata;
                 myChart.update();
-            if(JSON.stringify(myChart.config._config.data.datasets)!==JSON.stringify(results.data.chartdata.datasets)){
-                console.log("neu data");
+            if(JSON.stringify(myChart.config._config.data.datasets)===JSON.stringify(results.data.chartdata.datasets)){
+                console.log(myChart.config._config.data.datasets);
+                console.log(results.data.chartdata.datasets);
             }else{
-                console.log("alt data");
+                console.log("neu");
+                console.log(myChart.config._config.data.datasets);
+                console.log(results.data.chartdata.datasets);
 
             }
 
