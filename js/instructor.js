66cac0f1267877b1122ace52e162443c8f97a3e7
Momo <immxmmi@gmx.at>
2022-06-17 15:22:11 +0200

    try reload func

--- js/instructor.js
+++ js/instructor.js
@@ -809,9 +809,10 @@ function create_chart(chart_typ){
 
 
 function update_chart(chart_typ){
-    console.log(myChart.data)
-    console.log(myChart.data.data)
-    console.log(myChart.data.options)
+    console.log(myChart.data.labels)
+
+
+
 
     /*
 
