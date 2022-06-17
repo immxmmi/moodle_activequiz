b5b4eead8a51ae05e0fdc6aeec44429ed3f14497
Momo <immxmmi@gmx.at>
2022-06-17 16:00:15 +0200

    try reload func

--- js/instructor.js
+++ js/instructor.js
@@ -818,7 +818,7 @@ function update_chart(chart_typ){
             url: '' + url + '?sessionid=' + activequiz.get('sessionid') + '&type=' + chart_typ + '',
             dataType: 'json',
         }).done(function (results) {
-            if(myChart.config._config.data.labels.equals(results.data.chartdata.labels)){
+            if(JSON.stringify(myChart.config._config.data.labels)===JSON.stringify(results.data.chartdata.labels)){
                 console.log("mayer")
             }else{
                 console.log(results.data.chartdata.labels)
