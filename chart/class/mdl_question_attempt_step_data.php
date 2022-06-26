68d4a283a101d7ac9425ac1af266b7ad3279807e
Momo <immxmmi@gmx.at>
2022-06-26 23:07:25 +0200

    hotfix 2.0

--- chart/class/mdl_question_attempt_step_data.php
+++ chart/class/mdl_question_attempt_step_data.php
@@ -25,7 +25,7 @@ class attempt_step_data
                     $this->id = $data->id;
                     $this->attemptstepid = $data->attemptstepid;
                     $this->name = $data->name;
-                    $this->value = implode(',', $data->value);
+                    echo implode(',', $data->value);
                 }
     }
 
