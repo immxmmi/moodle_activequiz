cae2278149cd8f3511935ba59b8f5cc6563a8005
Momo <immxmmi@gmx.at>
2022-06-26 18:34:54 +0200

    hotfix 2.0

--- chart/class/chart_builder.php
+++ chart/class/chart_builder.php
@@ -17,7 +17,7 @@ class chart_builder
     }
 
     //CREATE NEW CHART RETURN JSON
-    public function build_new_chart($chartType, $labels, $values, $msg)
+    public function build_new_chart($chartType, $labels, $values, $msg2)
     {
         $this->chartType = $chartType;
 
@@ -43,7 +43,7 @@ class chart_builder
                         )
                     )
                 );
-                $this->msg .= json_encode($msg);
+                $this->msg = json_encode($msg2);
                 break;
             case 'pie':
                 $this->data = array(
