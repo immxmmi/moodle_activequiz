e4bead20e44588bb6b03db8f8d166a30b7432146
Momo <immxmmi@gmx.at>
2022-06-26 21:21:29 +0200

    hotfix 2.0

--- chart/chart_api.php
+++ chart/chart_api.php
@@ -59,7 +59,7 @@ foreach ($question_attemp as $qA) {
     //  array_push($list_of_question_attempts_id, $qA->getId());
 */
     echo "<pre>";
-    print_r($qA);
+    print_r($qA[0]);
     echo "</pre>";
 
  }
