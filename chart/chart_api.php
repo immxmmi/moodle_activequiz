b51982335921a26362e2b7256d93498484f46a7f
Momo <immxmmi@gmx.at>
2022-06-26 20:48:16 +0200

    hotfix 2.0

--- chart/chart_api.php
+++ chart/chart_api.php
@@ -1,10 +1,10 @@
 <?php
     require_once("../../../config.php");
-  //require_once("class/mdl_activequiz_sessions.php");
-  //require_once("class/mdl_activequiz_attempt.php");
-  //require_once("class/mdl_question_attempts.php");
-  //require_once("class/mdl_question_attempt_steps.php");
-  //require_once("class/mdl_question_attempt_step_data.php");
+    require_once("class/mdl_activequiz_sessions.php");
+    require_once("class/mdl_activequiz_attempt.php");
+    require_once("class/mdl_question_attempts.php");
+    require_once("class/mdl_question_attempt_steps.php");
+    require_once("class/mdl_question_attempt_step_data.php");
   //require_once("class/chart_builder.php");
   //require_once("class/single_choice.php");
   //require_once("class/truefalsechoice.php");
