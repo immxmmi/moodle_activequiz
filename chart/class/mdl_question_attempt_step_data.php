d18bd0867f6fe128ae38d63401cb9649bb996011
Momo <immxmmi@gmx.at>
2022-06-26 22:16:19 +0200

    hotfix 2.0

--- chart/class/mdl_question_attempt_step_data.php
+++ chart/class/mdl_question_attempt_step_data.php
@@ -24,6 +24,7 @@ class attempt_step_data
         echo"<pre>";
         print_r($step_data);
         echo"</pre>";
+        /*
                 foreach ($step_data as $data) {
                     $this->id = $data->id;
                     $this->attemptstepid = $data->attemptstepid;
@@ -31,6 +32,7 @@ class attempt_step_data
                     $this->value = $data->value;
                     $this->answer = $data->answer;
                 }
+        */
     }
 
 
