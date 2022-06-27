65fe162e59317d989a14b9cdf8bd5b3a272e71d3
Momo <immxmmi@gmx.at>
2022-06-27 11:56:51 +0200

    hotfix 2.0

--- chart/class/multi_choice.php
+++ chart/class/multi_choice.php
@@ -82,6 +82,9 @@ class multi_choice
         $answers = array();
         foreach ($list_of_user_question_data as $current_user_data){
               //  print_r($current_user_data["_order"]);
+
+            print_r($list_of_user_question_data);
+            /*
             for($i = 0; $i < sizeof($current_user_data)-1; $i++){
                 $value = $current_user_data["choice".$i][0];
                 if($value){
@@ -89,6 +92,7 @@ class multi_choice
                 }
 
             }
+            */
         }
 
         return $answers;
