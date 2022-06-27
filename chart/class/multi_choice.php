99b1464aed873bbc2c79fe4424383416e8f8c1fc
Momo <immxmmi@gmx.at>
2022-06-27 09:37:51 +0200

    hotfix 2.0

--- chart/class/multi_choice.php
+++ chart/class/multi_choice.php
@@ -43,7 +43,6 @@ class multi_choice
         $listAnswer = array();
         foreach ($steps_data as $data){
                 array_push($listAnswer, $data->getAnswerList());
-            break;
         }
 
         echo "<pre>";
