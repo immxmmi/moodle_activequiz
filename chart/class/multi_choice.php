b008247fef7d2d447dbef3bb3d965f7e4d01a152
Momo <immxmmi@gmx.at>
2022-06-27 11:12:08 +0200

    hotfix 2.0

--- chart/class/multi_choice.php
+++ chart/class/multi_choice.php
@@ -101,7 +101,7 @@ class multi_choice
         foreach ($list_of_user_question_data as $current_user_data){
 
             for($i = 0; $i < sizeof($current_user_data)-1; $i++){
-                echo $i;
+                echo $current_user_data["choice".$i];
             }
 
             echo "<pre>";
